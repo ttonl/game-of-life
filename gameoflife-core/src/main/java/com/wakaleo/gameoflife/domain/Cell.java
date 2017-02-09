7c8746b2d8c92a60d0f35bba3526ece52f307601
ttonl <tto@nl-it-consulting.de>
2017-02-09 13:23:24 +0100

    Added funny comment

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -2,7 +2,8 @@ package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
-
+	
+	//Ohhhhhhhhh private String :D
     private String symbol;
 
     private Cell(final String initialSymbol) {
